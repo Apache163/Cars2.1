14cefb5955779a778c369f984f8608b9a3b83abc
Apache163 <skd_010701@bk.ru>
2022-04-07 14:58:16 +0400

    Убрал подготовленные заранее запросы + Убрал лишние строки в mainwindow.cpp

--- Cars_Database/mainwindow.cpp
+++ Cars_Database/mainwindow.cpp
@@ -51,90 +51,66 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    QString str= textEdit->toplainText();
+    QString str = ui->textEdit->toPlainText();
+    qDebug() << "You typed: " + str + " in TextEdit.";
+
+
+
+
 
     if (ui->radioButton->isChecked())
     {
-        rb = 1;
+        query = new QSqlQuery(db);
+        query->prepare("select * from aboutcar where CAR_ID = " + str);
+        model->query();
     }
     if (ui->radioButton_2->isChecked())
     {
-        rb = 2;
+        model->setFilter("BRAND = 'Tesla'");
+        model->select();
     }
     if (ui->radioButton_3->isChecked())
     {
-        rb = 3;
+        model->setFilter("MODEL = 'XC70'");
+        model->select();
     }
     if (ui->radioButton_4->isChecked())
     {
-        rb = 4;
+        query = new QSqlQuery(db);
+        query->prepare("select * from aboutcar where MODEL_YEAR = " + str);
+        model->query();
+        /*model->setFilter("MODEL_YEAR = 2019");
+        model->select();*/
     }
     if (ui->radioButton_5->isChecked())
     {
-        rb = 5;
+        query = new QSqlQuery(db);
+        query->prepare("select * from aboutcar where FIRST_MODEL_YEAR = " + str);
+        model->query();
+        /*model->setFilter("FIRST_MODEL_YEAR = 2004");
+        model->select();*/
     }
     if (ui->radioButton_6->isChecked())
     {
-        rb = 6;
+        model->setFilter("BODY_TYPE = 'Jeep'");
+        model->select();
     }
     if (ui->radioButton_7->isChecked())
     {
-        rb = 7;
+        query = new QSqlQuery(db);
+        query->prepare("select * from aboutcar where DOORS = " + str);
+        model->query();
+        /*model->setFilter("DOORS = 5");
+        model->select();*/
     }
     if (ui->radioButton_8->isChecked())
     {
-        rb = 8;
+        query = new QSqlQuery(db);
+        query->prepare("select * from aboutcar where ENGINE_POWER = " + str);
+        model->query();
+        /*model->setFilter("ENGINE_POWER = 560");
+        model->select();*/
     }
-    switch(rb)
-    {
-        case 1:
-        model->setFilter("CAR_ID = 4");
-        model->select();
-        //запрос;
-        break;
-        case 2:
-        model->setFilter("BRAND = 'Tesla'");
-        model->select();
-        //запрос;
-        break;
-        case 3:
-        model->setFilter("MODEL = 'XC70'");
-        model->select();
-        //запрос;
-        break;
-        case 4:
-        model->setFilter("MODEL_YEAR = 2019");
-        model->select();
-        //запрос;
-        break;
-        case 5:
-        model->setFilter("FIRST_MODEL_YEAR = 2004");
-        model->select();
-        //запрос;
-        break;
-        case 6:
-        model->setFilter("BODY_TYPE = 'Jeep'");
-        model->select();
-        //запрос;
-        break;
-        case 7:
-        model->setFilter("DOORS = 5");
-        model->select();
-        //запрос;
-        break;
-        case 8:
-        model->setFilter("ENGINE_POWER = 560");
-        model->select();
-        //запрос;
-        break;
-        }
-
-   std::cout << rb;
-
-
- //Добавить switch-case на проверку радиокнопок и отправки соответствующего запроса
-    //ui->radioClear->isChecked() - проверка на нажатие
-    //запрос типа select * from aboutcar where КОЛОНКА = выделенная ячейка в таблице
 
 }
 
