d4a55a51860cec3a99290ba64f012ebc7b8134dc
kirill <kirill@astra>
2022-07-19 15:03:04 +0400

    First Astra Commit

--- Cars_Database/mainwindow.cpp
+++ Cars_Database/mainwindow.cpp
@@ -7,6 +7,7 @@
 #include <QFileDialog>
 #include <QFile>
 #include <QDir>
+#include <QTableWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -57,51 +58,51 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked() //Запросы по поиску общей информации об авто
+void MainWindow::on_pushButton_FindDB1_clicked() //Запросы по поиску общей информации об авто
 {
-    QString str = ui->textEdit->toPlainText();
+    QString str = ui->textEdit_SearchBarBrand->toPlainText();
     qDebug() << "You typed: " + str + " in TextEdit";
-    ui->textEdit->clear();
+    ui->textEdit_SearchBarBrand->clear();
 
-    if (ui->radioButton->isChecked())
+    if (ui->radioButton_CarID1->isChecked())
     {
         model->setFilter("CAR_ID = "+str);
         model->select();
     }
-    if (ui->radioButton_2->isChecked())
+    if (ui->radioButton_Brand1->isChecked())
     {
         qr = "BRAND = '"+str+"'";
         model->setFilter(qr);
         model->select();
     }
-    if (ui->radioButton_3->isChecked())
+    if (ui->radioButton_Model1->isChecked())
     {
         qr = "MODEL = '"+str+"'";
         model->setFilter(qr);
         model->select();
     }
-    if (ui->radioButton_4->isChecked())
+    if (ui->radioButton_ModelYear1->isChecked())
     {
         model->setFilter("MODEL_YEAR = "+str);
         model->select();
     }
-    if (ui->radioButton_5->isChecked())
+    if (ui->radioButton_FirstModel1->isChecked())
     {
         model->setFilter("FIRST_MODEL_YEAR = "+str);
         model->select();
     }
-    if (ui->radioButton_6->isChecked())
+    if (ui->radioButton_BodyType1->isChecked())
     {
         qr = "BODY_TYPE = '"+str+"'";
         model->setFilter(qr);
         model->select();
     }
-    if (ui->radioButton_7->isChecked())
+    if (ui->radioButton_Doors1->isChecked())
     {
         model->setFilter("DOORS = "+str);
         model->select();
     }
-    if (ui->radioButton_8->isChecked())
+    if (ui->radioButton_Power1->isChecked())
     {
         model->setFilter("ENGINE_POWER = "+str);
         model->select();
@@ -109,17 +110,19 @@ void MainWindow::on_pushButton_clicked() //Запросы по поиску об
     qDebug() << qr;
 }
 
-void MainWindow::on_pushButton_2_clicked()
+void MainWindow::on_pushButton_UpdateDB1_clicked()
 {
-    ui->textEdit->clear();
+    ui->textEdit_SearchBarBrand->clear();
     query = new QSqlQuery(db);
     model = new QSqlTableModel(this, db);
     model->setTable("ABOUT_CARS");
+
+
     model->select();
     ui->tableView->setModel(model);
 }
 
-void MainWindow::on_pushButton_3_clicked()
+void MainWindow::on_pushButton_Exit1_clicked()
 {
     this->close();
 }
@@ -127,67 +130,67 @@ void MainWindow::on_pushButton_3_clicked()
 
 
 
-void MainWindow::on_pushButton_4_clicked() //Запросы по поиску конкретного авто
+void MainWindow::on_pushButton_FindDB2_clicked() //Запросы по поиску конкретного авто
 {
-    QString str = ui->textEdit_2->toPlainText();
+    QString str = ui->textEdit_SearchBarCC->toPlainText();
     qDebug() << "You typed: " + str + " in TextEdit";
-    ui->textEdit_2->clear();
+    ui->textEdit_SearchBarCC->clear();
 
-    if (ui->radioButton_9->isChecked())
+    if (ui->radioButton_CCID2->isChecked())
     {
         model2->setFilter("CC_ID = "+str);
         model2->select();
     }
-    if (ui->radioButton_10->isChecked())
+    if (ui->radioButton_CarID2->isChecked())
     {
         model2->setFilter("CAR_ID = "+str);
         model2->select();
     }
-    if (ui->radioButton_11->isChecked())
+    if (ui->radioButton_Brand2->isChecked())
     {
         qr = "BRAND = '"+str+"'";
         model2->setFilter(qr);
         model2->select();
     }
-    if (ui->radioButton_12->isChecked())
+    if (ui->radioButton_Model2->isChecked())
     {
         qr = "MODEL = '"+str+"'";
         model2->setFilter(qr);
         model2->select();
     }
-    if (ui->radioButton_13->isChecked())
+    if (ui->radioButton_ModelYear2->isChecked())
     {
         model2->setFilter("MODEL_YEAR = "+str);
         model2->select();
     }
-    if (ui->radioButton_14->isChecked())
+    if (ui->radioButton_FirstModel2->isChecked())
     {
         model2->setFilter("FIRST_MODEL_YEAR = "+str);
         model2->select();
     }
-    if (ui->radioButton_15->isChecked())
+    if (ui->radioButton_BodyType2->isChecked())
     {
         qr = "BODY_TYPE = '"+str+"'";
         model2->setFilter(qr);
         model2->select();
     }
-    if (ui->radioButton_16->isChecked())
+    if (ui->radioButton_BodyColor2->isChecked())
     {
         qr = "BODY_COLOR = '"+str+"'";
         model2->setFilter(qr);
         model2->select();
     }
-    if (ui->radioButton_17->isChecked())
+    if (ui->radioButton_Mileage2->isChecked())
     {
         model2->setFilter("MILEAGE <= "+str);
         model2->select();
     }
-    if (ui->radioButton_18->isChecked())
+    if (ui->radioButton_Doors2->isChecked())
     {
         model2->setFilter("DOORS = "+str);
         model2->select();
     }
-    if (ui->radioButton_19->isChecked())
+    if (ui->radioButton_Power2->isChecked())
     {
         model2->setFilter("ENGINE_POWER = "+str);
         model2->select();
@@ -198,13 +201,13 @@ void MainWindow::on_pushButton_4_clicked() //Запросы по поиску к
 }
 
 
-void MainWindow::on_pushButton_6_clicked()
+void MainWindow::on_pushButton_Exit2_clicked()
 {
     this->close();
 }
 
 
-void MainWindow::on_pushButton_5_clicked()
+void MainWindow::on_pushButton_UpdateDB2_clicked()
 {
     query2 = new QSqlQuery(db);
     model2 = new QSqlTableModel(this, db);
@@ -214,27 +217,27 @@ void MainWindow::on_pushButton_5_clicked()
 }
 
 
-void MainWindow::on_pushButton_7_clicked() //Запросы по поиску владельца и его автомобилей
+void MainWindow::on_pushButton_FindDB3_clicked() //Запросы по поиску владельца и его автомобилей
 {
-    QString str = ui->textEdit_3->toPlainText();
+    QString str = ui->textEdit_SearchBarOwners->toPlainText();
     qDebug() << "You typed: " + str + " in TextEdit";
-    ui->textEdit_3->clear();
+    ui->textEdit_SearchBarOwners->clear();
 
-    if (ui->radioButton_20->isChecked())
+    if (ui->radioButton_FullName->isChecked())
     {
         qr = "FullName = '"+str+"'";
         model3->setFilter(qr);
         model3->select();
     }
 
-    if (ui->radioButton_21->isChecked())
+    if (ui->radioButton_Email->isChecked())
     {
         qr = "Email = '"+str+"'";
         model3->setFilter(qr);
         model3->select();
     }
 
-    if (ui->radioButton_22->isChecked())
+    if (ui->radioButton_CarsByFullName->isChecked())
     {
         qr = "FullName = '"+str+"'";
         ui->tableView_3->setModel(model4);
@@ -242,12 +245,12 @@ void MainWindow::on_pushButton_7_clicked() //Запросы по поиску в
         model4->select();
     }
 
-    if (ui->radioButton_23->isChecked())
+    if (ui->radioButton_OwnerID->isChecked())
     {
         model3->setFilter("OwnerId = "+str);
         model3->select();
     }
-    if (ui->radioButton_24->isChecked())
+    if (ui->radioButton_CarsByEmail->isChecked())
     {
         qr = "Email = '"+str+"'";
         ui->tableView_3->setModel(model4);
@@ -259,7 +262,7 @@ void MainWindow::on_pushButton_7_clicked() //Запросы по поиску в
 }
 
 
-void MainWindow::on_pushButton_8_clicked()
+void MainWindow::on_pushButton_UpdateDB3_clicked()
 {
     query3 = new QSqlQuery(db);
     model3 = new QSqlTableModel(this, db);
@@ -271,19 +274,19 @@ void MainWindow::on_pushButton_8_clicked()
 }
 
 
-void MainWindow::on_pushButton_9_clicked()
+void MainWindow::on_pushButton_Exit3_clicked()
 {
     this->close();
 }
 
 
-void MainWindow::on_pushButton_12_clicked()
+void MainWindow::on_pushButton_ConnectDB1_clicked()
 {
-    QString File = QFileDialog::getOpenFileName(this,tr("Сhoose DataBase"),"C://","All file (*.*);;");
+    QString File = QFileDialog::getOpenFileName(this,tr("Сhoose DataBase"),"Домашний//","All file (*.*);;");
     qDebug() << File;
     if (QFile::exists(File))
     {
-        QString FilePath = "E://Work//Cars2.0//Cars_Database//"+ File.right(File.size()-File.lastIndexOf("/")-1); //QCoreApplication::applicationDirPath()
+        QString FilePath = "Домашний//Cars2.0//Cars_Database"+ File.right(File.size()-File.lastIndexOf("/")-1); //QCoreApplication::applicationDirPath()
         qDebug() << FilePath;
         qDebug() << QFile::copy(File, FilePath);
     }
@@ -334,13 +337,13 @@ void MainWindow::on_pushButton_12_clicked()
 }
 
 
-void MainWindow::on_pushButton_11_clicked()
+void MainWindow::on_pushButton_ConnectDB2_clicked()
 {
-    QString File = QFileDialog::getOpenFileName(this,tr("Сhoose DataBase"),"C://","All file (*.*);;");
+    QString File = QFileDialog::getOpenFileName(this,tr("Сhoose DataBase"),"Домашний//Cars2.0//Cars_Database","All file (*.*);;");
     qDebug() << File;
     if (QFile::exists(File))
     {
-        QString FilePath = "E://Work//Cars2.0//Cars_Database//"+ File.right(File.size()-File.lastIndexOf("/")-1); //QCoreApplication::applicationDirPath()
+        QString FilePath = "Домашний//Cars2.0//Cars_Database"+ File.right(File.size()-File.lastIndexOf("/")-1); //QCoreApplication::applicationDirPath()
         qDebug() << FilePath;
         qDebug() << QFile::copy(File, FilePath);
     }
@@ -385,13 +388,13 @@ void MainWindow::on_pushButton_11_clicked()
 }
 
 
-void MainWindow::on_pushButton_10_clicked()
+void MainWindow::on_pushButton_ConnectDB3_clicked()
 {
-    QString File = QFileDialog::getOpenFileName(this,tr("Сhoose DataBase"),"C://","All file (*.*);;");
+    QString File = QFileDialog::getOpenFileName(this,tr("Сhoose DataBase"),"Домашний//","All file (*.*);;");
     qDebug() << File;
     if (QFile::exists(File))
     {
-        QString FilePath = "E://Work//Cars2.0//Cars_Database//"+ File.right(File.size()-File.lastIndexOf("/")-1); //QCoreApplication::applicationDirPath()
+        QString FilePath = "Домашний//Cars2.0//Cars_Database"+ File.right(File.size()-File.lastIndexOf("/")-1); //QCoreApplication::applicationDirPath()
         qDebug() << FilePath;
         qDebug() << QFile::copy(File, FilePath);
     }
@@ -435,3 +438,113 @@ void MainWindow::on_pushButton_10_clicked()
     model4->select();
 }
 
+
+void MainWindow::on_pushButton_AddToDB1_clicked()
+{
+    ui->tabWidget->setCurrentIndex(3);
+}
+
+void MainWindow::on_pushButton_AddToDB2_clicked()
+{
+    ui->tabWidget->setCurrentIndex(3);
+}
+
+void MainWindow::on_pushButton_AddToDB3_clicked()
+{
+    ui->tabWidget->setCurrentIndex(3);
+}
+
+void MainWindow::on_radioButton_AddToBrandList_clicked() //Brands
+{
+    ui->tableWidget->setColumnCount(8);
+    ui->tableWidget->setRowCount(1);
+    ui->tableWidget->setHorizontalHeaderLabels(QStringList() << "Car_ID" << "Brand" << "Model"
+                                               << "Model_Year" << "First_Model" << "Body_type"
+                                               << "Doors" << "Engine_Power");
+    ui->tableWidget->clearContents();
+}
+
+void MainWindow::on_radioButton_AddToCarsList_clicked() //Current Cars
+{
+    ui->tableWidget->setColumnCount(11);
+    ui->tableWidget->setRowCount(1);
+    ui->tableWidget->setHorizontalHeaderLabels(QStringList() <<"CC_ID" << "Car_ID" << "Brand" << "Model"
+                                               << "Model_Year" << "First_Model" << "Body_type" << "Body_Color"
+                                               << "Mileage" << "Doors" << "Engine_Power");
+    ui->tableWidget->clearContents();
+}
+
+void MainWindow::on_radioButton_AddToOwnersList_clicked() //Owners
+{
+    ui->tableWidget->setColumnCount(3);
+    ui->tableWidget->setRowCount(1);
+    ui->tableWidget->setHorizontalHeaderLabels(QStringList() << "Owner_ID" << "Full_Name" << "Email");
+    ui->tableWidget->clearContents();
+}
+
+void MainWindow::on_pushButton_Exit4_clicked()
+{
+    this->close();
+}
+
+void MainWindow::on_pushButton_AddQuery_clicked()
+{
+   if (ui->radioButton_AddToBrandList->isChecked())//brands
+   {
+
+        car_id = ui->tableWidget->item(0,0)->text();
+        brand = ui->tableWidget->item(0,1)->text();
+        car_model = ui->tableWidget->item(0,2)->text();
+        model_year = ui->tableWidget->item(0,3)->text();
+        first_model = ui->tableWidget->item(0,4)->text();
+        body_type = ui->tableWidget->item(0,5)->text();
+        doors = ui->tableWidget->item(0,6)->text();
+        engine_power = ui->tableWidget->item(0,7)->text();
+        QSqlQuery query5 = QSqlQuery(db);
+        query5.exec("INSERT INTO ABOUT_CARS (CAR_ID, BRAND, MODEL, MODEL_YEAR, FIRST_MODEL_YEAR, BODY_TYPE, DOORS, ENGINE_POWER)"
+                    "VALUES ("+ car_id +", '" + brand + "', '" + car_model + "', " + model_year + ", " + first_model + ", '" +
+                   body_type + "', " + doors+", " + engine_power + ")");
+
+
+
+   }
+   if (ui->radioButton_AddToCarsList->isChecked())//Current cars
+   {
+
+       cc_id = ui->tableWidget->item(0,0)->text();
+       car_id = ui->tableWidget->item(0,1)->text();
+       brand = ui->tableWidget->item(0,2)->text();
+       car_model = ui->tableWidget->item(0,3)->text();
+       model_year = ui->tableWidget->item(0,4)->text();
+       first_model = ui->tableWidget->item(0,5)->text();
+       body_type = ui->tableWidget->item(0,6)->text();
+       body_color = ui->tableWidget->item(0,7)->text();
+       mileage = ui->tableWidget->item(0,8)->text();
+       doors = ui->tableWidget->item(0,9)->text();
+       engine_power = ui->tableWidget->item(0,10)->text();
+
+       QSqlQuery query6 = QSqlQuery(db);
+       query6.exec("INSERT INTO currentcar (CC_ID, CAR_ID, BRAND, MODEL, CC_MODEL_YEAR, FIRST_MODEL_YEAR, BODY_TYPE, BODY_COLOR, MILEAGE, DOORS, ENGINE_POWER)"
+                   "VALUES (" + cc_id + ", " + car_id + ", '" + brand + "', '" + car_model + "', " + model_year + ", " + first_model + ", '" + body_type + "', '" +
+                   body_color + "', " + mileage + ", " + doors + ", " + engine_power + ")");
+   }
+
+   if (ui->radioButton_AddToOwnersList->isChecked())//Owners
+   {
+
+       owner_id = ui->tableWidget->item(0,0)->text();
+       full_name = ui->tableWidget->item(0,1)->text();
+       email = ui->tableWidget->item(0,2)->text();
+       QSqlQuery query7 = QSqlQuery(db);
+       query7.exec("INSERT INTO owner (OwnerId, FullName, Email) VALUES (" + owner_id + ", '" + full_name + "', '" + email + "')");
+       QSqlQuery query8 = QSqlQuery(db);
+       query8.exec("INSERT INTO QueryHelper (BRAND, MODEL, CC_MODEL_YEAR, FIRST_MODEL_YEAR,  BODY_TYPE, BODY_COLOR, MILEAGE, DOORS, ENGINE_POWER, FullName, Email)"
+                   "VALUES ('"+brand+"', '"+car_model+"', "+model_year+", " + first_model + ", '" + body_type + "', '" +
+                   body_color + "', " + mileage + ", " + doors + ", " + engine_power+", '"+full_name+"', '"+email+"')");
+   }
+}
+
+void MainWindow::on_pushButton_Clear_clicked()
+{
+    ui->tableWidget->clearContents();
+}
--- Cars_Database/mainwindow.h
+++ Cars_Database/mainwindow.h
@@ -26,36 +26,49 @@ private slots:
 
 
 
+    void on_pushButton_Exit1_clicked();
 
 
 
+    void on_pushButton_FindDB2_clicked();
 
+    void on_pushButton_Exit2_clicked();
 
-    void on_pushButton_clicked();
+    void on_pushButton_UpdateDB2_clicked();
 
-    void on_pushButton_2_clicked();
+    void on_pushButton_FindDB3_clicked();
 
-    void on_pushButton_3_clicked();
+    void on_pushButton_UpdateDB3_clicked();
 
+    void on_pushButton_Exit3_clicked();
 
+    void on_pushButton_ConnectDB1_clicked();
 
-    void on_pushButton_4_clicked();
+    void on_pushButton_ConnectDB2_clicked();
 
-    void on_pushButton_6_clicked();
+    void on_pushButton_ConnectDB3_clicked();
 
-    void on_pushButton_5_clicked();
+    void on_pushButton_AddToDB1_clicked();
 
-    void on_pushButton_7_clicked();
+    void on_pushButton_AddToDB2_clicked();
 
-    void on_pushButton_8_clicked();
+    void on_pushButton_AddToDB3_clicked();
 
-    void on_pushButton_9_clicked();
+    void on_radioButton_AddToBrandList_clicked();
 
-    void on_pushButton_12_clicked();
+    void on_radioButton_AddToCarsList_clicked();
 
-    void on_pushButton_11_clicked();
+    void on_radioButton_AddToOwnersList_clicked();
 
-    void on_pushButton_10_clicked();
+    void on_pushButton_Exit4_clicked();
+
+    void on_pushButton_AddQuery_clicked();
+
+    void on_pushButton_Clear_clicked();
+
+    void on_pushButton_FindDB1_clicked();
+
+    void on_pushButton_UpdateDB1_clicked();
 
 private:
     Ui::MainWindow *ui;
@@ -70,10 +83,26 @@ private:
     QSqlTableModel *model4;
 
 
+
+
     int rb;
     QString str;
     QString qr;
     QString File;
+    QString car_id;
+    QString model_year;
+    QString first_model;
+    QString doors;
+    QString engine_power;
+    QString cc_id;
+    QString mileage;
+    QString owner_id;
+    QString brand;
+    QString car_model;
+    QString body_type;
+    QString body_color;
+    QString full_name;
+    QString email;
 
 };
 #endif // MAINWINDOW_H
